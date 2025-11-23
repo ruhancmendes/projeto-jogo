49d80965e1dc7c00b89b208b5c745a157229d428
ruhancmendes <ruhancmendes@gmail.com>
2025-11-22 21:57:19 -0300

    commit: project game

---
+++ src/App.tsx
@@ -0,0 +1,151 @@
+import styles from "./app.module.css";
+import { useEffect, useState } from "react";
+
+import { WORDS } from "./utils/words";
+import type { Challenge } from "./utils/words";
+
+import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { LettersUsed } from "./components/LettersUsed";
+import type { LettersUsedProps } from "./components/LettersUsed";
+
+const ATTEMPTS_MARGIN = 5;
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [letter, setLetter] = useState("");
+  const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
+  function handleRestartGame() {
+    const isConfirmed = window.confirm("Você tem certeza que deseja reiniciar o jogo?")
+
+    if(isConfirmed){
+      startGame()
+    }
+  }
+
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+
+    setChallenge(randomWord);
+
+    setScore(0);
+    setLetter("");
+    setLettersUsed([]);
+  }
+
+  function handleConfirm() {
+    if (!challenge) {
+      return;
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!");
+    }
+
+    const value = letter.toUpperCase();
+    const exists = lettersUsed.find(
+      (used) => used.value.toUpperCase() === value
+    );
+
+    if (exists) {
+      setLetter("")
+      return alert("Você já tentou a letra " + value);
+    }
+
+    const hits = challenge.word
+      .toUpperCase()
+      .split("")
+      .filter((char) => char === value).length;
+
+    const correct = hits > 0;
+    const currentScore = score + hits;
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }]);
+    setScore(currentScore);
+    setLetter("");
+  }
+
+  function endGame(message: string) {
+    alert(message);
+    startGame();
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  useEffect(() => {
+    if (!challenge) {
+      return;
+    }
+
+    setTimeout(() => {
+      if (score === challenge.word.length) {
+        return endGame("Parabéns! Você descobriu a palavra!");
+      }
+
+      const attemptLimit = challenge.word.length + ATTEMPTS_MARGIN;
+
+      if (lettersUsed.length === attemptLimit) {
+        return endGame(
+          "Que pena, suas tentativas acabaram! A palavra era: " + challenge.word
+        );
+      }
+    }, 200);
+  }, [score, lettersUsed.length]);
+
+  if (!challenge) {
+    return;
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header
+          current={lettersUsed.length}
+          max={challenge.word.length + ATTEMPTS_MARGIN}
+          onRestart={handleRestartGame}
+        />
+
+        <Tip tip={challenge.tip} />
+
+        <div className={styles.word}>
+          {challenge.word.split("").map((letter, index) => {
+            const letterUsed = lettersUsed.find(
+              (used) => used.value.toUpperCase() === letter.toUpperCase()
+            );
+
+            return (
+              <Letter
+                key={index}
+                value={letterUsed?.value}
+                color={letterUsed?.correct ? "correct" : "default"}
+              />
+            );
+          })}
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
+        </div>
+
+        <LettersUsed data={lettersUsed} />
+      </main>
+    </div>
+  );
+}
